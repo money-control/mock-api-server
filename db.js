5a0af769542f8ed3b55c49c523d54e6180224b76
suryasahu <suryanarayan.sahu@nw18.com>
2025-08-20 21:15:05 +0530

    chore: Code fix

--- db.js
+++ db.js
@@ -13,6 +13,22 @@ const stockCompareData = require('./data/stock/compare/data.json');
 const homeTrendingData = require('./data/home/trending/data.json');
 const proStockListConfig = require('./data/pro-stock-list/config.json');
 const homeProStockListConfig = require('./data/home/pro-stock-list/config.json');
+const unlistedConsumption = require('./data/unlisted-shares/landing.json');
+const unlistedOverview = require('./data/unlisted-shares/overview/overview.json');
+const unlistedOverviewDetails = require('./data/unlisted-shares/overview/details.json');
+const unlistedOverviewPartner = require('./data/unlisted-shares/overview/partner.json');
+const unlistedOverviewPrice = require('./data/unlisted-shares/overview/price.json');
+const unlistedOverviewReturn= require('./data/unlisted-shares/overview/return.json');
+const unlistedOverviewKeyStats= require('./data/unlisted-shares/overview/keystatistics.json');
+const unlistedOverviewFinOverview= require('./data/unlisted-shares/overview/financeoverview.json');
+const unlistedInfoMgmt= require('./data/unlisted-shares/info-management/info.json');
+const unlistedShareHolding= require('./data/unlisted-shares/share-holding/share-holding.json');
+const unlistedShareHoldingTop= require('./data/unlisted-shares/share-holding/share-top-holding.json');
+const unlistedCorporateAction= require('./data/unlisted-shares/corporate-action/corporateaction.json');
+const unlistedCorporateActionBonus= require('./data/unlisted-shares/corporate-action/bonus.json');
+const unlistedCorporateActionDividend= require('./data/unlisted-shares/corporate-action/dividend.json');
+const unlistedCorporateActionReturn= require('./data/unlisted-shares/corporate-action/return.json');
+const unlistedCorporateActionSplit= require('./data/unlisted-shares/corporate-action/splits.json');
 
 module.exports = {
   users,
@@ -27,6 +43,22 @@ module.exports = {
   stockCompareData,
   homeTrendingData,
   proStockListConfig,
-  homeProStockListConfig
+  homeProStockListConfig,
+  unlistedConsumption,
+  unlistedOverview,
+  unlistedOverviewDetails,
+  unlistedOverviewPartner,
+  unlistedOverviewPrice,
+  unlistedOverviewReturn,
+  unlistedOverviewKeyStats,
+  unlistedOverviewFinOverview,
+  unlistedInfoMgmt,
+  unlistedShareHolding,
+  unlistedShareHoldingTop,
+  unlistedCorporateAction,
+  unlistedCorporateActionBonus,
+  unlistedCorporateActionDividend,
+  unlistedCorporateActionReturn,
+  unlistedCorporateActionSplit
 };
 
