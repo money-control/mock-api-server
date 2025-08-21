d15df840d6799145298448ee0ae06a856bd7bd4a
suryasahu <suryanarayan.sahu@nw18.com>
2025-08-21 10:13:21 +0530

    Revert "chore: Code fix"
    This reverts commit 3956a600eb01365765f6332a48e01c7b24a6ee40.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
-const jsonServer = require('./node_modules/json-server/lib/server');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(require('./db.js'));
 const middlewares = jsonServer.defaults();
